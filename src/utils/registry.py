dfaea78c3ec139fd4b31500839acf6e17aee9c47
jkskw <jakub.skwierczynski15@gmail.com>
2025-04-06 22:02:07 +0200

    Update model registry

--- src/utils/registry.py
+++ src/utils/registry.py
@@ -5,5 +5,12 @@ MODEL_REGISTRY = {
     "ResNet3D_18": "src.model.models.resnet3d",
     "ResNet3D_34": "src.model.models.resnet3d",
     "ResNet3D_50": "src.model.models.resnet3d",
-    "ResNet3D_101": "src.model.models.resnet3d"
+    "ResNet3D_101": "src.model.models.resnet3d",
+    "ResNeXt3D_50": "src.model.models.resnext3d",
+    "DenseNet3D_121": "src.model.models.densenet3d",
+    "CustomNet": "src.model.models.customnet",
+    "EnhancedSpinal3DNet": "src.model.models.enhanced_spinal_3dnet",
+    "ConvNeXtSmallLSTM": "src.model.models.convnext_lstm",
+    "AdvancedSpinal3DNetResNetEncoder": "src.model.models.resnet_encoder",
+    "ResNeXtEncoderBiLSTMClassifier": "src.model.models.resnext_lstm"
 }
